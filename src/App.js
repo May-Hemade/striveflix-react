67939d7ff8850b64223e0a957e2fe9cc8cd70124
May-Hemade <may.hemade1993@gmail.com>
2021-12-17 16:57:21 +0200

    fixed the css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import MovieCarousel from "./component/MovieCarousel"
+import { Container } from "react-bootstrap"
+import { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Container fluid>
+          <h3 className="py-3">Lord of The Rings</h3>
+          <MovieCarousel query="lord of the rings" />
+          <h3 className="py-3">Harry Potter</h3>
+          <MovieCarousel query="harry potter" />
+          <h3 className="py-3">Star Wars</h3>
+          <MovieCarousel query="star wars" />
+        </Container>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
---
+++ src/component/MovieCarousel.jsx
@@ -0,0 +1,72 @@
+import { Component } from "react"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Carousel from "react-bootstrap/Carousel"
+import MovieCard from "./MovieCard"
+
+class MovieCarousel extends Component {
+  state = {
+    movies: [],
+  }
+
+  getMovies = async () => {
+    try {
+      let response = await fetch(
+        `http://www.omdbapi.com/?apikey=4440f104&s=${this.props.query}`
+      )
+
+      if (response.ok) {
+        let result = await response.json()
+        let movieList = result.Search
+        this.setState({ movies: movieList })
+      } else {
+        console.log(`something went wrong Don't Panic`)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  componentDidMount = () => {
+    this.getMovies()
+  }
+  arrayChunks = (inputArray, perChunk) => {
+    var result = inputArray.reduce((resultArray, item, index) => {
+      const chunkIndex = Math.floor(index / perChunk)
+
+      if (!resultArray[chunkIndex]) {
+        resultArray[chunkIndex] = []
+      }
+
+      resultArray[chunkIndex].push(item)
+
+      return resultArray
+    }, [])
+    return result
+  }
+
+  render() {
+    return (
+      <Carousel fade indicators={false}>
+        {this.arrayChunks(this.state.movies, 6).map((moviesRow, index) => (
+          <Carousel.Item
+            key={`carousel-item-${index}`}
+            className="d-flex inline"
+          >
+            <Row className="row w-100 mx-auto" role="listbox">
+              {moviesRow.map((movie) => (
+                <Col key={movie.imdbID} xs={12} s={6} md={3} lg={2}>
+                  <MovieCard
+                    movie={movie}
+                    showComments={this.props.showComments}
+                  ></MovieCard>
+                </Col>
+              ))}
+            </Row>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    )
+  }
+}
+
+export default MovieCarousel
